7f3912586191a17e7e279b96a7f3d7e8084aadad
cameronapak <cameronandrewpak@gmail.com>
2025-06-01 06:44:22 -0500

    Add Prettier and update backend configuration
    - Introduced Prettier and Prettier plugin for Astro as devDependencies in package.json and package-lock.json for consistent code formatting.
    - Enhanced backend configuration in index.ts by updating the database connection type and adding initial data seeding for posts and comments.

--- src/utils/bknd/index.ts
+++ src/utils/bknd/index.ts
@@ -1,9 +1,56 @@
+import type { ModuleBuildContext } from "bknd";
 import { type AstroBkndConfig, getApp as getBkndApp } from "bknd/adapter/astro";
+import { em, entity, text, number } from "bknd/data";
+import { randomString } from "bknd/utils";
+
+const schema = em({
+  posts: entity("posts", {
+    // "id" is automatically added
+    title: text().required(),
+    slug: text().required(),
+    content: text(),
+    views: number()
+  }),
+  comments: entity("comments", {
+    content: text()
+  })
+
+  // relations and indices are defined separately.
+  // the first argument are the helper functions, the second the entities.
+}, ({ relation, index }, { posts, comments }) => {
+  relation(comments).manyToOne(posts);
+  // relation as well as index can be chained!
+  index(posts).on(["title"]).on(["slug"], true);
+});
 
 export const config = {
   connection: {
-    url: "file:data.db",
+    type: "libsql",
+    config: {
+      // location of your local Astro DB
+      // make sure to use a remote URL in production
+      url: "file:.astro/content.db"
+    }
   },
+  initialConfig: {
+    data: schema.toJSON(),
+    auth: {
+      enabled: true,
+      jwt: {
+        secret: randomString(64)
+      }
+    }
+  },
+  app: {
+    options: {
+      seed: async (ctx: ModuleBuildContext) => {
+        await ctx.em.mutator("posts").insertMany([
+           { title: "First post", slug: "first-post", content: "..." },
+           { title: "Second post", slug: "second-post" }
+        ]);
+     }
+    },
+  }
 } as const satisfies AstroBkndConfig;
 
 export async function getApp() {
